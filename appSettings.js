3c6aacfe2481aa77cb588fa53a2072e417342e40
KarthikCC27 <Karthikeyan_Karunanithi@comcast.com>
2024-02-28 11:02:17 +0530

    first commit

---
+++ appSettings.js
@@ -0,0 +1,11 @@
+const settings = {
+    'clientId': '4b0c21cc-d19e-4835-8cb8-b156e9124926',
+    'tenantId': 'common',
+    'graphUserScopes': [
+      'user.read',
+      'mail.read',
+      'mail.send'
+    ]
+  };
+  
+  module.exports = settings;
